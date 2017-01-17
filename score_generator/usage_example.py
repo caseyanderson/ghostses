b5af09ffaa8eb39cf59c432d4d5f5ff3bc6110de
caseyanderson <casey.thomas.anderson@gmail.com>
2017-01-16 21:10:55 -0800

    have to use margins, regen'd pdfs

--- score_generator/usage_example.py
+++ score_generator/usage_example.py
@@ -21,7 +21,7 @@ newblah = wordTkzCrps(blah)
 
 corpus = tupleSplitter(newblah)
 
-output = colorizer(corpus[0], corpus[1], 'rest', dctnry )
+output = colorizer(corpus[0], corpus[1], 'adj', dctnry )
 
 allhtml = whitespacer(output)
 
