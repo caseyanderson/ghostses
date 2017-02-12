53e6f9521512918015ec6520ed2c5de6572da568
caseyanderson <casey.thomas.anderson@gmail.com>
2017-02-11 22:07:35 -0800

    made a background.html, ignoring sass, etc.

--- score_generator/py/usage.py
+++ score_generator/py/usage.py
@@ -5,18 +5,18 @@ import re
 from bs4 import BeautifulSoup
 import time
 
-corpus = "/score_generator/py/corpus.txt"
-out = "/score_generator/html/"
+corpus = "score_generator/py/corpus.txt"
+out = "score_generator/html/"
 
 #refactor the next two lines
 dctnry={}
-keys = ['noun', 'adj', 'vrb', 'advrb','symb']
+keys = ['noun', 'adj', 'vrb', 'advrb','symb', 'background']
 
 dctnry['noun'] = [ 'NN', 'NNP', 'NNPS', 'NNS']
 dctnry['adj'] = ['JJ', 'JJR', 'JJS']
 dctnry['vrb'] = ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
 dctnry['advrb'] = ['RB', 'RBR', 'RBS']
-dctnry['rest'] = ['CC', 'CD', 'DT', 'EX', 'FW', 'IN', 'LS', 'MD', 'PDT', 'POS', 'PRP', 'PRP$', 'RP', 'SYM', 'TO', 'UH', 'WDT', 'WP', 'WP$', 'WRP']
+dctnry['background'] = ['CC', 'CD', 'DT', 'EX', 'FW', 'IN', 'LS', 'MD', 'PDT', 'POS', 'PRP', 'PRP$', 'RP', 'SYM', 'TO', 'UH', 'WDT', 'WP', 'WP$', 'WRP']
 dctnry['symb'] = ['$', "''", '(', ')', ',', '--', '.', ':', "''" ]
 
 blah = readCorpus(corpus)
@@ -26,7 +26,7 @@ newblah = wordTkzCrps(blah)
 corpus = tupleSplitter(newblah)
 
 
-output = colorizer(corpus[0], corpus[1], str(keys[2]), dctnry)
+output = colorizer(corpus[0], corpus[1], str(keys[5]), dctnry)
 allhtml = whitespacer(output)
-path = out + str(keys[2]) + '.html'
+path = out + str(keys[5]) + '.html'
 final = outputer(path, allhtml)
