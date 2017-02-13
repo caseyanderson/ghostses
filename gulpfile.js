b8aaf9cf31050026b40d9d0a8caef1a50dd52273
caseyanderson <casey.thomas.anderson@gmail.com>
2017-02-13 13:55:54 -0800

    localhost url gets made automatically now

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,19 @@ var connect = require('gulp-connect');
 var sass = require('gulp-sass');
 var open = require('gulp-open');
 
-  var config = {
-      port: 9005,
-      url: 'http://localhost',
-      html: './score_generator/html/*.html',
-      css: './score_generator/css/',
-      scss: './score_generator/scss/styles.scss'
-  }
+var config = {
+    port: 9005,
+    url: 'http://localhost',
+    html: './score_generator/html/*.html',
+    css: './score_generator/css/',
+    scss: './score_generator/scss/styles.scss',
+    base: '/score_generator/html/'
+}
+
+var sassOptions = {
+    errLogToConsole: true,
+    outputStyle: 'expanded'
+};
 
 gulp.task('connect', function() {
     connect.server({
@@ -26,12 +32,12 @@ gulp.task('html', function () {
 
 gulp.task('open', function(){
     gulp.src('./score_generator/html/')
-        .pipe(open({uri: 'http://localhost:9005/score_generator/'}));
+        .pipe(open({uri: ((config.url).concat(':', config.port, config.base )) }));
 });
 
 gulp.task('styles', function() {
     gulp.src(config.scss)
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass(sassOptions).on('error', sass.logError))
         .pipe(gulp.dest(config.css))
         .pipe(connect.reload());
 });
