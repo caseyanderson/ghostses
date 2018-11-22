610fe0a78a7fcb374e003542af57a8a79cf3ba4f
caseyanderson <cta@caseyanderson.com>
2018-11-22 12:51:42 -0800

    all the changes

--- score_generator/py/usage.py
+++ score_generator/py/usage.py
@@ -1,9 +1,15 @@
-test = Ghostses('corpus_mini.txt')
-test.readCorpus()
-# test.getTokens()
-# test.getPOS()
+# making ghostses
 
-#refactor the next two lines
+
+## setup
+
+score = Ghostses('corpus.txt') # make the score text object, set the corpus filepath
+score.readCorpus() # read the corpus into object
+score.getTokens(spaces=True) # get tokens and preserve spaces
+score.getPOS() # perform parts of speech analysis on non-whitespace tokens
+
+
+# makethe colorizer dictionary
 dctnry={}
 keys = ['noun', 'adj', 'vrb', 'advrb','symb', 'background']
 
@@ -13,3 +19,9 @@ dctnry['vrb'] = ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
 dctnry['advrb'] = ['RB', 'RBR', 'RBS']
 dctnry['background'] = ['CC', 'CD', 'DT', 'EX', 'FW', 'IN', 'LS', 'MD', 'PDT', 'POS', 'PRP', 'PRP$', 'RP', 'SYM', 'TO', 'UH', 'WDT', 'WP', 'WP$', 'WRP']
 dctnry['symb'] = ['$', "''", '(', ')', ',', '--', '.', ':', "''" ]
+
+for i in keys:
+    print('making ' + str(i))
+    score.colorizer(str(i), dctnry)
+    score.assembler(str(i))
+    score.renderer(str(i))
