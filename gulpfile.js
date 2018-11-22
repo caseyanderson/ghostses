01a9221ef9f6e6e50a81c655e32d53fc9644daef
caseyanderson <cta@caseyanderson.com>
2018-11-22 12:13:55 -0800

    fixed gulp shit

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,24 @@
+'use strict';
+
+var os = require('os');
 var gulp = require('gulp');
-var connect = require('gulp-connect');
-var sass = require('gulp-sass');
-var open = require('gulp-open');
-
-var config = {
-    port: 9005,
-    url: 'http://localhost',
-    html: './score_generator/html/*.html',
-    css: './score_generator/css/',
-    scss: './score_generator/scss/styles.scss',
-    base: './score_generator/html/'
-}
-
-var sassOptions = {
-    errLogToConsole: true,
-    outputStyle: 'expanded'
-};
+var  connect = require('gulp-connect');
 
 gulp.task('connect', function() {
-    connect.server({
-        port: config.port,
-        base: config.url,
-        livereload: true
-    });
+  connect.server({
+    root: 'score_generator',
+    livereload: true
+  });
 });
 
 gulp.task('html', function () {
-    gulp.src(config.html)
+  gulp.src('./score_generator/html/*.html')
+    .pipe(gulp.dest('./score_generator/html/'))
     .pipe(connect.reload());
 });
 
-gulp.task('open', function(){
-    gulp.src(config.base)
-        .pipe(open({uri: (config.url).concat(':', config.port, (config.base).replace('.','')) }));
-});
-
-gulp.task('styles', function() {
-    gulp.src(config.scss)
-        .pipe(sass(sassOptions).on('error', sass.logError))
-        .pipe(gulp.dest(config.css))
-        .pipe(connect.reload());
-});
-
 gulp.task('watch', function () {
-    gulp.watch(config.html, ['html']);
-    gulp.watch(config.scss, ['styles']);
+  gulp.watch(['./score_generator/html/*.html'], ['html']);
 });
 
-gulp.task('default', ['connect', 'open', 'watch']);
+gulp.task('default', ['connect', 'watch']);
