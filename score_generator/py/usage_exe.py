863f61295204a8222af0dedd9324da4fd8eb8e83
caseyanderson <cta@caseyanderson.com>
2018-11-22 20:05:34 -0800

    there

--- score_generator/py/usage_exe.py
+++ score_generator/py/usage_exe.py
@@ -19,25 +19,25 @@ if __name__ == '__main__':
     parser.add_argument("--ws", type=bool, default=False,
                         help="preserve whitespace")
     args = parser.parse_args()
-    
+
     ## setup
-    
+
     score = Ghostses(args.crps) # make the score text object, set the corpus filepath
     score.readCorpus() # read the corpus into object
     score.getTokens(spaces=args.ws) # get tokens and preserve spaces
     score.getPOS() # perform parts of speech analysis on non-whitespace tokens
-    
+
     # makethe colorizer dictionary
     dctnry={}
     keys = ['noun', 'adj', 'vrb', 'advrb','symb', 'background']
-    
+
     dctnry['noun'] = [ 'NN', 'NNP', 'NNPS', 'NNS']
     dctnry['adj'] = ['JJ', 'JJR', 'JJS']
     dctnry['vrb'] = ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
     dctnry['advrb'] = ['RB', 'RBR', 'RBS']
     dctnry['background'] = ['CC', 'CD', 'DT', 'EX', 'FW', 'IN', 'LS', 'MD', 'PDT', 'POS', 'PRP', 'PRP$', 'RP', 'SYM', 'TO', 'UH', 'WDT', 'WP', 'WP$', 'WRP']
     dctnry['symb'] = ['$', "''", '(', ')', ',', '--', '.', ':', "''" ]
-    
+
     for i in keys:
         print('making ' + str(i))
         score.colorizer(str(i), dctnry)
