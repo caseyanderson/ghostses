cf215085faf610271e104b0fa334d6d03979d92e
caseyanderson <cta@caseyanderson.com>
2018-11-23 13:27:44 -0800

    added comments to make_ghostses.py, started outline in readme.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ghostses
+
+the software
+
+
+
+the piece
--- layer_generator/py/make_ghostses.py
+++ layer_generator/py/make_ghostses.py
@@ -13,12 +13,14 @@ import argparse
 
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--crps", type=str, default="../../corpora/ch1_RoS/ch1_RoS_edited.txt",
                         help="the corpus")
     parser.add_argument("--ws", type=bool, default=False,
                         help="preserve whitespace")
     args = parser.parse_args()
+
 
     ## setup
 
@@ -27,9 +29,14 @@ if __name__ == '__main__':
     score.getTokens(spaces=args.ws) # get tokens and preserve spaces
     score.getPOS() # perform parts of speech analysis on non-whitespace tokens
 
+
     # makethe colorizer dictionary
+
     dctnry={}
     keys = ['noun', 'adj', 'vrb', 'advrb','symb', 'background']
+
+
+    # possible tags per each part of speech category
 
     dctnry['noun'] = [ 'NN', 'NNP', 'NNPS', 'NNS']
     dctnry['adj'] = ['JJ', 'JJR', 'JJS']
@@ -38,8 +45,11 @@ if __name__ == '__main__':
     dctnry['background'] = ['CC', 'CD', 'DT', 'EX', 'FW', 'IN', 'LS', 'MD', 'PDT', 'POS', 'PRP', 'PRP$', 'RP', 'SYM', 'TO', 'UH', 'WDT', 'WP', 'WP$', 'WRP']
     dctnry['symb'] = ['$', "''", '(', ')', ',', '--', '.', ':', "''" ]
 
+
+    # make all of the layers, output to /layer_generator/html/
+
     for i in keys:
         print('making ' + str(i))
-        score.colorizer(str(i), dctnry)
-        score.assembler(str(i))
-        score.renderer(str(i))
+        score.colorizer(str(i), dctnry) # add html tags
+        score.assembler(str(i)) # combine spaces with processed list
+        score.renderer(str(i)) # format and write the html file
