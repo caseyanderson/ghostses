7525d46f2aec4bdf00cf44ab8fac77c214d14472
caseyanderson <cta@caseyanderson.com>
2018-11-25 10:43:01 -0800

    added gulp-open, gulp-sass, and sass:watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,11 @@
 
 var os = require('os');
 var gulp = require('gulp');
-var  connect = require('gulp-connect');
+var connect = require('gulp-connect');
+var sass = require('gulp-sass');
+var open = require('gulp-open');
+
+sass.compiler = require('node-sass');
 
 gulp.task('connect', function() {
   connect.server({
@@ -21,4 +25,22 @@ gulp.task('watch', function () {
   gulp.watch(['./layer_generator/html/*.html'], ['html']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+
+gulp.task('sass', function () {
+  return gulp.src('./layer_generator/scss/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./layer_generator/css'))
+    .pipe(connect.reload());
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./layer_generator/scss/*.scss', ['sass']);
+});
+
+
+gulp.task('open', function() {
+  gulp.src('')
+    .pipe(open({uri: 'http://localhost:8080/html/'}));
+});
+
+gulp.task('default', ['open', 'connect', 'watch', 'sass:watch']);
